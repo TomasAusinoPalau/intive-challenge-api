8b136bfe1116545c0013cd4131190fd85a53a8fe
Tomas <tausinopalau@gmail.com>
2021-08-22 23:01:02 -0300

    detail-view desktop, responsive design, readme

--- readme.md
+++ readme.md
@@ -14,3 +14,24 @@ With that array, using a loop, I made a grid of users with thumbnail, name and l
 I didn't use jQuery to generate the grid in the DOM, because I consider it a bad practice. But it would have been a good option to save time, and solve it in less lines of code.
 
 At this point, with the grid, I can use the `onClick` event to open the detail view from an specific selected user and manipulate his properties, through the `dataRandomUser[index]`. The `handleClick` function recives the event and shows the detail view.
+
+When all this things were settled, I can start the detail view with the user's picture, name and location.
+
+Developer features that i used:
+- Vanilla JS
+- material.io
+- Google Fonts
+- Mobile First
+- Responsive Design
+
+
+I founded few problems that limited myself to keep the code clean and simple.
+
+- I couldn't divide the code in different files, because I didn't know how to modularize the code in different little archives. But i tried to simplify readability with few functions.
+- I could make a infinite scroll, but I find troubles in the API request and the way how I handle the data, so every time I scroll down and call the api for new random users, it overwrite and modify the array `dataRandomUsers` and its original index positions.
+- I cannon't control the `exitButton` class 'hidden', in a way that also allows (if the user is in desktop view) select other user directly from the grid. When that happens, the `exitButton` dissapears. If the user is with the phone rotated (desktop view) and after selected a second user rotates the phone, he cannon't see the exit button, because the `exitButton` is hidden, the user will go back to the grid, but the button is not visible.
+
+
+## Final thoughts
+
+I think that in more time I would made a better handle of the data, and a better way to handle the API request, plus the possibility to make a infinite scroll. 
---
+++ src/main.js
@@ -0,0 +1,189 @@
+let API_RESULTS_1 = 50;
+let dataRandomUsers
+
+
+const url = `https://randomuser.me/api/?results=${API_RESULTS_1}`;
+
+const fetchAPI = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    dataRandomUsers = data.results
+
+    userGenerator()
+};
+
+
+
+const userGenerator = () => {
+    for (let i = 0; i < dataRandomUsers.length; i++) {
+        generateUser(i, dataRandomUsers[i]);
+    };
+}
+
+const generateUser = (index, user) => {
+        
+
+
+    const userData = user;
+    const { name, picture, location } = userData;
+    const { title, first, last } = name;
+    const {thumbnail} = picture;
+    const {city, country} = location;
+
+    const parentElement = document.querySelector('.main-container');
+    
+    
+
+    const userElement = elementCreator('div');
+    const userNameElement = elementCreator('p')
+    const userLocationElement = elementCreator('p');
+    const userPictureElement = elementCreator('picture');
+    const userThumbnailElement = elementCreator('img');
+    const userSeparatorElement = elementCreator('hr');
+
+
+
+
+
+    userNameElement.innerHTML = `${title} ${first} ${last}`;
+    userLocationElement.innerHTML = `${city}, ${country}`;
+    userThumbnailElement.src = `${thumbnail}`;
+
+
+
+
+
+    elementAddClass(userNameElement, 'user-name')
+    elementAddClass(userElement, 'user-container')
+    elementAddClass(userLocationElement, 'user-location')
+    elementAddClass(userPictureElement, 'user-picture-container')
+    elementAddClass(userThumbnailElement, 'user-thumbnail')
+    elementAddClass(userSeparatorElement, 'user-separator')
+
+
+    userElement.onclick = (event) => {handleUserClick(event.target, index)};
+
+    
+    
+    userPictureElement.appendChild(userThumbnailElement)
+
+    userElement.appendChild(userPictureElement)
+    userElement.appendChild(userNameElement);
+    userElement.appendChild(userLocationElement);
+    userElement.appendChild(userSeparatorElement);
+    
+    parentElement.appendChild(userElement);
+
+}
+
+const handleUserClick = (event, index) => {
+
+    const userData = dataRandomUsers[index];
+    const detailsParent = document.querySelector('.details-container');
+    
+    const thumbnailUserClass = 'user-thumbnail';
+    const thumbnailClick = event.parentNode.parentNode
+    const textClick = event.parentNode
+    const userSelectedContainer = (event.className === thumbnailUserClass) ? thumbnailClick : textClick;
+
+
+    if(detailsParent.children.length !== 0) {
+        detailsParent.innerHTML = ''
+    }
+
+
+    const { name, picture, location, email, login } = userData;
+    const {large} = picture
+    const {username} = login
+
+    
+
+    
+
+    const userDetailsElements = userSelectedContainer.cloneNode(true);
+    const userUsernameElement = elementCreator('p');
+    const userEmailElement = elementCreator('p');
+    const userPictureContainer = elementCreator('picture');
+    const userImgElement = elementCreator('img');
+    userImgElement.src = large;
+    
+
+
+    detailsParent.classList.remove('hidden')
+    elementAddClass(userDetailsElements, 'user-details');
+    elementAddClass(userUsernameElement, 'user-username');
+    elementAddClass(userEmailElement, 'user-email');
+    elementAddClass(userPictureContainer, 'user-picture-container');
+    elementAddClass(userImgElement, 'user-picture');
+
+    renderExitButton()
+
+
+
+    appendDetails({
+                userDetails: userData,
+                parentElement: detailsParent,
+                baseElement: userDetailsElements,
+                usernameElement: userUsernameElement, 
+                emailElement: userEmailElement,
+                pictureContainer: userPictureContainer,
+                imageElement: userImgElement
+            });
+
+    
+
+
+}
+
+const elementCreator = (element) => {
+    return document.createElement(element);
+}
+
+
+function elementAddClass(element, className) {
+
+    element.classList.add(className);
+}
+
+const renderExitButton = () => {
+    const exitButton = document.querySelector('.exit-button');
+    const exitIcon = document.querySelector('.exit-icon');
+    exitButton.classList.contains('hidden') ? exitButton.classList.remove('hidden') : exitButton.classList.add('hidden');
+    exitIcon.classList.contains('hidden')   ? exitIcon.classList.remove('hidden') : exitIcon.classList.add('hidden');
+
+    exitButton.onclick = event => {handleExitButton(event)};    
+}
+
+
+
+const handleExitButton = (event) => {
+    const detailsParent = document.querySelector('.details-container');
+    detailsParent.classList.add('hidden')
+    detailsParent.innerHTML = ''
+    renderExitButton()
+
+}
+
+const appendDetails = (details) => {
+    const {userDetails, parentElement, baseElement, usernameElement, emailElement, pictureContainer, imageElement } = details;
+    const {picture, email, login} = userDetails;
+    const {username} = login
+    const {large} = picture;
+    
+
+
+    usernameElement.innerHTML = `Username: ${username}`;
+    emailElement.innerHTML = `Email: ${email}`;
+    imageElement.src = `${large}`;
+    imageElement.alt = `${username}`;
+    imageElement.title = `${username}`;
+
+    pictureContainer.appendChild(imageElement)
+    baseElement.removeChild(baseElement.querySelector('.user-picture-container'))
+    baseElement.appendChild(usernameElement);
+    baseElement.appendChild(emailElement);
+    baseElement.appendChild(pictureContainer);
+    parentElement.appendChild(baseElement);
+}
+
+fetchAPI()
